59ffe762badaaad8f890fa169bf4031eea7c96a8
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-02-10 05:15:36 +0100

    update MQTT example

--- MQTT/main.py
+++ MQTT/main.py
@@ -2,13 +2,17 @@ import network
 from umqtt.simple import MQTTClient
 import time
 
+time.sleep(1)
+
 WLAN_SSID = 'WLAN_AP2'
 WLAN_PSK = 'your password'
 wlan = network.WLAN(network.STA_IF)
 wlan.scan()
 wlan.active(True)
-wlan.connect(WLAN_SSID, WLAN_PSK)
 
+while not wlan.isconnected():
+    wlan.connect(WLAN_SSID, WLAN_PSK)
+    wlan.scan()
 
 MQTT_BROKER = '10.0.0.111'
 MQTT_PORT = 1883
@@ -17,8 +21,7 @@ publisher.connect()
 
 
 for i in range(0, 10):
-    publisher.publish(b'home/test', b'hello world')
-    print(i)
+    publisher.publish(b'home/test', b'hello world: {}'.format(i))
     time.sleep(2)
 
 publisher.disconnect()
