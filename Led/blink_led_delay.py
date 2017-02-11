1f69cf972b9271aa0746c3e543aa01a2f80f4f9e
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-02-11 05:26:20 +0100

    update led examples

--- Led/blink_led_delay.py
+++ Led/blink_led_delay.py
@@ -1,15 +1,15 @@
 from machine import Pin
 import time
 
-p4 = Pin(4, Pin.OUT)
+led = Pin(14, Pin.OUT)
 
 def toggle():
-    global p4
-    if p4.value():
-        p4.low()
+    global led
+    if led.value():
+        led.low()
     else:
-        p4.high()
+        led.high()
 
-for i range(0, 10):
+for i in range(0, 10):
     toggle()
     time.sleep(2)  # sleep two seconds
--- Led/blink_led_timer.py
+++ Led/blink_led_timer.py
@@ -1,14 +1,14 @@
 from machine import Pin, Timer
 
-p4 = Pin(4, Pin.OUT)
+led = Pin(14, Pin.OUT)
 timer = Timer(-1)
 
 def toggle():
-    global p4
-    if p4.value():
-        p4.low()
+    global led
+    if led.value():
+        led.low()
     else:
-        p4.high()
+        led.high()
 
 # toggles the led every two seconds
 timer.init(period=2000, mode=Timer.PERIODIC, callback=lambda t: toggle())
